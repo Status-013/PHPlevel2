f0e48a822f259c92b09c93d320169178a18a9bf5
Status-013 <status-13@mail.ru>
2023-03-21 21:31:33 +0300

    hw4

--- cli.php
+++ cli.php
@@ -9,16 +9,16 @@ include __DIR__ . "/vendor/autoload.php";
 //Создаём объект подключения к SQLite
 $connection = new PDO('sqlite:' . __DIR__ . '/blog.sqlite');
 
-$usersRepository = new SqliteUsersRepository($connection);
+//$usersRepository = new SqliteUsersRepository($connection);
 $postsRepository = new SqlitePostsRepository($connection);
 
 try {
 
-$user = $usersRepository->get(new UUID('3b697686-01bf-433a-bf17-53ce84cb987b'));
+//$user = $usersRepository->get(new UUID('3b697686-01bf-433a-bf17-53ce84cb987b'));
 
-$post = $postsRepository->get(new UUID("fb58c755-9613-4945-b1d7-b2bd1979ae34"));
+$post = $postsRepository->get(new UUID("fb58c755-9413-4945-b1d7-b2bd1979ae34"));
 
-print_r($post);
+echo($post);
 
 } catch (Exception $e) {
     echo $e->getMessage();
